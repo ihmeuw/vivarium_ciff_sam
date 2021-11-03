8ccaee33bb24acfb784395bf7bf58fbc7be484ac
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2021-11-03 12:11:16 -0700

    refactor x factor exposure

--- src/vivarium_ciff_sam/components/__init__.py
+++ src/vivarium_ciff_sam/components/__init__.py
@@ -1,7 +1,7 @@
-from vivarium_ciff_sam.components.disease_effect import DiseaseEffect
 from vivarium_ciff_sam.components.intervention import SQLNSIntervention, WastingTreatmentIntervention
 from vivarium_ciff_sam.components.observers import (CategoricalRiskObserver, DisabilityObserver, DiseaseObserver,
                                                     MortalityObserver)
 from vivarium_ciff_sam.components.lbwsg import LowBirthWeight, ShortGestation
 from vivarium_ciff_sam.components.treatment import SQLNSTreatment
 from vivarium_ciff_sam.components.wasting import ChildWasting
+from vivarium_ciff_sam.components.x_factor import XFactorExposure
