8671e1c9fdb40f60b5a265d3ed305640ed3d4f94
rmudambi <11376379+rmudambi@users.noreply.github.com>
2021-12-02 18:02:43 -0800

    update conditional x-factor exposure for paf calculation (#75)

--- src/vivarium_ciff_sam/components/__init__.py
+++ src/vivarium_ciff_sam/components/__init__.py
@@ -4,4 +4,4 @@ from vivarium_ciff_sam.components.observers import (CategoricalRiskObserver, Dis
 from vivarium_ciff_sam.components.lbwsg import LowBirthWeight, ShortGestation
 from vivarium_ciff_sam.components.treatment import SQLNSTreatment, WastingTreatment
 from vivarium_ciff_sam.components.wasting import ChildWasting
-from vivarium_ciff_sam.components.x_factor import XFactorExposure
+from vivarium_ciff_sam.components.x_factor import XFactorExposure, XFactorEffect
--- src/vivarium_ciff_sam/components/x_factor.py
+++ src/vivarium_ciff_sam/components/x_factor.py
@@ -1,7 +1,10 @@
+from typing import Dict
+
 from vivarium.framework.engine import Builder
+from vivarium.framework.lookup import LookupTable
 from vivarium.framework.randomness import RandomnessStream
 
-from vivarium_public_health.risks import Risk
+from vivarium_public_health.risks import Risk, RiskEffect
 
 
 class XFactorExposure(Risk):
@@ -19,3 +22,49 @@ class XFactorExposure(Risk):
 
     def register_simulant_initializer(self, builder: Builder) -> None:
         pass
+
+
+class XFactorEffect(RiskEffect):
+
+    configuration_defaults = {
+        'effect_of_risk_on_target': {
+            'conditional_exposure': 0.0,
+            'measure': {
+                'relative_risk': None,
+                'mean': None,
+                'se': None,
+                'log_mean': None,
+                'log_se': None,
+                'tau_squared': None
+            }
+        }
+    }
+
+    def __init__(self, target: str):
+        super().__init__('risk_factor.x_factor', target)
+
+    ##########################
+    # Initialization methods #
+    ##########################
+
+    def get_configuration_defaults(self) -> Dict[str, Dict]:
+        return {
+            f'effect_of_{self.risk.name}_on_{self.target.name}': {
+                'conditional_exposure':
+                    XFactorEffect.configuration_defaults['effect_of_risk_on_target']['conditional_exposure'],
+                self.target.measure:
+                    XFactorEffect.configuration_defaults['effect_of_risk_on_target']['measure']
+            }
+        }
+
+    #################
+    # Setup methods #
+    #################
+
+    def get_population_attributable_fraction_source(self, builder: Builder) -> LookupTable:
+        source_key = f'effect_of_{self.risk.name}_on_{self.target.name}'
+        exposure = builder.configuration[source_key]['conditional_exposure']
+        rr = builder.configuration[source_key][self.target.measure]['relative_risk']
+
+        paf = exposure * (rr - 1) / (exposure * (rr - 1) + 1)
+        return builder.lookup.build_table(paf, key_columns=['sex'], parameter_columns=['age', 'year'])
