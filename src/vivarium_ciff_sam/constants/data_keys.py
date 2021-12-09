6075b173f7af40ec7ade0ed121c912e26e45644a
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2021-12-08 17:11:25 -0800

    load LBWSG rr interpolator into artifact

--- src/vivarium_ciff_sam/constants/data_keys.py
+++ src/vivarium_ciff_sam/constants/data_keys.py
@@ -258,6 +258,8 @@ class __LowBirthWeightShortGestation(NamedTuple):
     DISTRIBUTION: TargetString = 'risk_factor.low_birth_weight_and_short_gestation.distribution'
     CATEGORIES: TargetString = 'risk_factor.low_birth_weight_and_short_gestation.categories'
     RELATIVE_RISK: TargetString = 'risk_factor.low_birth_weight_and_short_gestation.relative_risk'
+    RELATIVE_RISK_INTERPOLATOR: TargetString = 'risk_factor.low_birth_weight_and_short_gestation.relative_risk_interpolator'
+
     PAF: TargetString = 'risk_factor.low_birth_weight_and_short_gestation.population_attributable_fraction'
 
     # Useful keys not for the artifact - distinguished by not using the colon type declaration
--- src/vivarium_ciff_sam/constants/metadata.py
+++ src/vivarium_ciff_sam/constants/metadata.py
@@ -1,3 +1,5 @@
+from typing import NamedTuple
+
 ####################
 # Project metadata #
 ####################
@@ -26,6 +28,27 @@ ARTIFACT_INDEX_COLUMNS = [
 GBD_2019_ROUND_ID = 6
 GBD_2020_ROUND_ID = 7
 
-GBD_2019_LBWSG_AGE_GROUPS = {164, 2, 3}
-GBD_2019_SIDS_AGE_GROUPS = {3}
-GBD_2020_AGE_GROUPS = {2, 3, 388, 389, 238, 34}
+
+class __AgeGroup(NamedTuple):
+    BIRTH_ID = 164
+    EARLY_NEONATAL_ID = 2
+    LATE_NEONATAL_ID = 3
+    MONTHS_1_TO_5 = 388
+    MONTHS_6_TO_11 = 389
+    MONTHS_12_TO_23 = 238
+    YEARS_2_TO_4 = 34
+
+    GBD_2019_LBWSG_EXPOSURE = {BIRTH_ID, EARLY_NEONATAL_ID, LATE_NEONATAL_ID}
+    GBD_2019_LBWSG_RELATIVE_RISK = {EARLY_NEONATAL_ID, LATE_NEONATAL_ID}
+    GBD_2019_SIDS = {LATE_NEONATAL_ID}
+    GBD_2020 = {
+        EARLY_NEONATAL_ID,
+        LATE_NEONATAL_ID,
+        MONTHS_1_TO_5,
+        MONTHS_6_TO_11,
+        MONTHS_12_TO_23,
+        YEARS_2_TO_4,
+    }
+
+
+AGE_GROUP = __AgeGroup()
