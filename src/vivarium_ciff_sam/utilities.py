0512f188ca046b1331a246ca173f99dd0ab29d2a
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2022-01-03 17:31:30 -0800

    fix truncnorm distribtion specification

--- src/vivarium_ciff_sam/utilities.py
+++ src/vivarium_ciff_sam/utilities.py
@@ -90,10 +90,13 @@ def get_norm_from_quantiles(mean: float, lower: float, upper: float,
 
 
 def get_truncnorm_from_quantiles(mean: float, lower: float, upper: float,
-                                 quantiles: Tuple[float, float] = (0.025, 0.975)) -> stats.truncnorm:
+                                 quantiles: Tuple[float, float] = (0.025, 0.975),
+                                 lower_clip: float = 0.0, upper_clip: float = 1.0) -> stats.truncnorm:
     stdnorm_quantiles = stats.norm.ppf(quantiles)
     sd = (upper - lower) / (stdnorm_quantiles[1] - stdnorm_quantiles[0])
-    return stats.truncnorm(loc=mean, scale=sd)
+    a = (lower_clip - mean) / sd if sd else 0.0
+    b = (upper_clip - mean) / sd if sd else 0.0
+    return stats.truncnorm(loc=mean, scale=sd, a=a, b=b)
 
 
 def get_lognorm_from_quantiles(median: float, lower: float, upper: float,
