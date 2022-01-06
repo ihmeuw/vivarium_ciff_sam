7d20678cf679a5d0cc1fde11fae26eb5d4a90493
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2022-01-06 14:24:35 -0800

    implement maternal malnutrition risk

--- src/vivarium_ciff_sam/components/__init__.py
+++ src/vivarium_ciff_sam/components/__init__.py
@@ -3,6 +3,7 @@ from vivarium_ciff_sam.components.mortality import Mortality
 from vivarium_ciff_sam.components.observers import (BirthObserver, CategoricalRiskObserver, DisabilityObserver,
                                                     DiseaseObserver, MortalityObserver)
 from vivarium_ciff_sam.components.lbwsg import LBWSGRisk, LBWSGRiskEffect, LBWSGSubRisk, LowBirthWeight, ShortGestation
+from vivarium_ciff_sam.components.maternal_malnutrition import AdditiveRiskEffect
 from vivarium_ciff_sam.components.treatment import SQLNSTreatment, WastingTreatment
 from vivarium_ciff_sam.components.wasting import ChildWasting
 from vivarium_ciff_sam.components.x_factor import XFactorExposure, XFactorEffect
---
+++ src/vivarium_ciff_sam/components/maternal_malnutrition.py
@@ -0,0 +1,69 @@
+from typing import Callable
+
+import pandas as pd
+
+from vivarium.framework.engine import Builder
+from vivarium.framework.lookup import LookupTable
+from vivarium_public_health.risks import RiskEffect
+
+from vivarium_ciff_sam.constants import data_keys
+
+
+class AdditiveRiskEffect(RiskEffect):
+
+    def __init__(self, risk: str, target: str):
+        super().__init__(risk, target)
+        self.target_risk_specific_shift_pipeline_name = f'{self.target.name}.risk_specific_shift'
+
+    def setup(self, builder: Builder) -> None:
+        super().setup(builder)
+        self._register_risk_specific_shift_modifier(builder)
+
+    def _get_target_modifier(self, builder: Builder) -> Callable[[pd.Index, pd.Series], pd.Series]:
+        risk_exposure = builder.value.get_value(f'{self.risk.name}.exposure')
+
+        def exposure_effect(target, rr: pd.DataFrame) -> pd.Series:
+            index_columns = ['index', self.risk.name]
+
+            exposure = risk_exposure(rr.index).reset_index()
+            exposure.columns = index_columns
+            exposure = exposure.set_index(index_columns)
+
+            relative_risk = rr.stack().reset_index()
+            relative_risk.columns = index_columns + ['value']
+            relative_risk = relative_risk.set_index(index_columns)
+
+            effect = relative_risk.loc[exposure.index, 'value'].droplevel(self.risk.name)
+            affected_rates = target + effect
+            return affected_rates
+
+        def adjust_target(index: pd.Index, target: pd.Series) -> pd.Series:
+            return exposure_effect(target, self.relative_risk(index))
+
+        return adjust_target
+
+    def _risk_specific_shift_source(self, builder: Builder) -> LookupTable:
+        risk_specific_shift_data = builder.data.load(
+            data_keys.MATERNAL_MALNUTRITION.RISK_SPECIFIC_SHIFT,
+            affected_entity=self.target.name,
+            affected_measure=self.target.measure
+        )
+        return builder.lookup.build_table(
+            risk_specific_shift_data,
+            key_columns=['sex'],
+            parameter_columns=['age', 'year']
+        )
+
+    def _register_paf_modifier(self, builder: Builder) -> None:
+        pass
+
+    def _register_risk_specific_shift_modifier(self, builder: Builder) -> None:
+        builder.value.register_value_modifier(
+            self.target_risk_specific_shift_pipeline_name,
+            modifier=self._risk_specific_shift_source,
+            requires_columns=['age', 'sex']
+        )
+
+    ##################################
+    # Pipeline sources and modifiers #
+    ##################################
